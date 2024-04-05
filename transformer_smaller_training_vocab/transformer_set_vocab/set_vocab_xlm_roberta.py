c59862e106515b4c55379545c80f4df85024284d
Benedikt Fuchs <benedikt.fuchs@rise-world.com>
2024-04-05 14:07:06 +0200

    update transformers set vocab to be compatible with newer versions

--- transformer_smaller_training_vocab/transformer_set_vocab/set_vocab_xlm_roberta.py
+++ transformer_smaller_training_vocab/transformer_set_vocab/set_vocab_xlm_roberta.py
@@ -9,4 +9,4 @@ from transformer_smaller_training_vocab.transformer_set_vocab.auto_set_vocab imp
 def set_xlm_roberta_vocab(tokenizer: XLMRobertaTokenizer, vocab: Dict[str, int]) -> None:
     tokenizer.fairseq_tokens_to_ids = vocab
     tokenizer.fairseq_ids_to_tokens = {v: k for k, v in tokenizer.fairseq_tokens_to_ids.items()}
-    tokenizer.added_tokens_encoder = {k: vocab[k] for k in tokenizer.added_tokens_encoder}
+    tokenizer.added_tokens_decoder = {vocab[k]: k for k in tokenizer.added_tokens_encoder}
